d94ff6f9c05e251c6697d3b8e9132c460c8751e3
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2022-05-25 06:44:36 -0400

    feat: add CLI

---
+++ README.md
@@ -0,0 +1,47 @@
+# No-Sweat™ Commitlint and Husky Setup
+
+This is my attempt at setting up a project/repo with Commitlint and Husky
+as fast as possible.
+
+## What it does
+
+- Commitlint (you guessed it) lints your comments based on [conventional commits' Standards](https://www.conventionalcommits.org/)
+- Husky uses githooks to lint the commits before they are actually executed (pre-commit hook)
+
+You are very welcome to overwrite any of these settings, or just fork the entire thing to create your own.
+
+## Installing
+
+1. If you don't already have a `package.json` file, create one with `npm init`.
+
+2. Then ensure you have a .git folder create one with `git init`
+
+3. In the root of your project execute the following command in your terminal:
+
+```
+npm i commitlint-with-husky --save-dev
+```
+4. Now add the configuration files to your project by running:
+
+```
+npx commitlint-with-husky
+```
+
+5. You can see in your package.json there are now 2 scripts added to your script property if you should need it.
+
+```json
+  "scripts": {
+  "your other scripts":"..."
+
+    "postpublish": "pinst --disable",
+    "prepublishOnly": "pinst --enable "
+  },
+```
+
+## NOTE
+
+I work on a windows machine, to the best of my knowledge the package works on all platforms, if not please don't hesitate to let me know.
+
+Enjoy!
+
+[MIT](https://github.com/clickwithclark/commitlint-with-husky/blob/main/LICENSE)
---
+++ bin/index.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const process = require("process");
+const fs = require("fs");
+const path = require("path");
+const { spawn } = require("child_process");
+const chalk = require("chalk");
+
+(function init() {
+  
+  
+  let userPkg =  require(path.resolve(process.cwd(),"./package.json"));
+  const configFile = require(path.resolve(__dirname,"../.commitlintrc.json"));
+
+    //shallow copy to avoid mutating user copy prematurely
+    userPkg =  JSON.parse(JSON.stringify(userPkg))
+
+    //append pinst scripts to user scripts in package.json
+    userPkg.scripts.postpublish ="pinst --disable" ;
+    userPkg.scripts.prepublishOnly ="pinst --enable" ;
+
+    //update package.json with merged scripts
+  try {
+    fs.writeFileSync(path.resolve(process.cwd(),"./package.json"), JSON.stringify(userPkg, null, 2));
+  } catch (error) {
+    console.log(chalk.bold.red("Error updating user package.json:\n"),error)
+  }
+    
+// move basic config file to user's project folder
+
+try {
+  fs.writeFileSync(path.resolve(process.cwd(),"./.commitlintrc.json"), JSON.stringify(configFile, null, 2));
+} catch (error) {
+  console.log(chalk.bold.red("Error copying over basic config file for Commitlint:\n"),error)
+}
+  
+ /*  execute postinstall script from package.json
+     to initialize user repo */
+
+    let child = spawn(String.raw`npx --yes husky install && npx --yes husky add .husky/commit-msg "npx --yes commitlint --edit \"$1\""`, { shell: true });
+  
+
+    child.stdout.on("data", (data) => {
+      console.log(chalk.bold.green("SUCCESS"),'\n',data.toString())
+    });
+    child.stderr.on("data", (data) => {
+      console.log(chalk.bold.red("FAILURE"),'\n',data.toString())
+    });
+  
+    child.on("error", (error) => {
+      console.log(chalk.bold.red("Error Message"),'\n',  error.message);
+      console.log(chalk.bold.yellow("Please Make sure you run npm i commitlint-with-husky first!! before npx commitlint-with-husky"));
+    });
+    
+    child.on('exit', function (exitCode) {
+      console.log(chalk.cyan(`Process exited with code: ${ exitCode}`));
+    });
+
+  })();
