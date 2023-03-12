d9bf4eebab0ec5682eba709bcc91709fe18ddba4
clickwithclark <20617584+clickwithclark@users.noreply.github.com>
2023-03-12 16:29:10 -0400

    build(package.json): installation triggers post install configuration settings
    NPX calling after default installation no longer required

--- bin/index.js
+++ bin/index.js
@@ -3,58 +3,56 @@
 const process = require("process");
 const fs = require("fs");
 const path = require("path");
-const { spawn } = require("child_process");
+const { exec } = require("child_process");
 const chalk = require("chalk");
 
 (function init() {
-  
-  
-  let userPkg =  require(path.resolve(process.cwd(),"./package.json"));
-  const configFile = require(path.resolve(__dirname,"../.commitlintrc.json"));
+  const userDirectory = process.env.INIT_CWD;
+  let userPkg = require(path.resolve(userDirectory, "./package.json"));
 
-    //shallow copy to avoid mutating user copy prematurely
-    userPkg =  JSON.parse(JSON.stringify(userPkg))
+  const configFile = require(path.resolve(__dirname, "../.commitlintrc.json"));
 
-    //append pinst scripts to user scripts in package.json
-    userPkg.scripts.postpublish ="pinst --disable" ;
-    userPkg.scripts.prepublishOnly ="pinst --enable" ;
-    userPkg.scripts.prepublishOnly ="pinst --enable" ;
 
-    //update package.json with merged scripts
+  //taken from https://www.youtube.com/live/2J9VnYiZ_Ts?feature=share
+  const huskyInstallation = String.raw`npx --yes husky install && npx --yes husky add .husky/commit-msg "npx --yes commitlint --edit \"$1\""`;
+
+  //shallow copy to avoid mutating user copy prematurely
+  userPkg = JSON.parse(JSON.stringify(userPkg));
+
+  //append pinst scripts to user scripts in package.json
+  userPkg.scripts.postpublish = "pinst --disable";
+  userPkg.scripts.prepublishOnly = "pinst --enable";
+
+  //update package.json with merged scripts
   try {
-    fs.writeFileSync(path.resolve(process.cwd(),"./package.json"), JSON.stringify(userPkg, null, 2));
+    fs.writeFileSync(
+      path.resolve(userDirectory, "./package.json"),
+      JSON.stringify(userPkg, null, 2)
+    );
   } catch (error) {
-    console.log(chalk.bold.red("Error updating user package.json:\n"),error)
+    console.log(chalk.bold.red("Error updating user package.json:\n"), error);
   }
-    
-// move basic config file to user's project folder
 
-try {
-  fs.writeFileSync(path.resolve(process.cwd(),"./.commitlintrc.json"), JSON.stringify(configFile, null, 2));
-} catch (error) {
-  console.log(chalk.bold.red("Error copying over basic config file for Commitlint:\n"),error)
-}
-  
- /*  execute postinstall script from package.json
-     to initialize user repo */
+  // move basic config file to user's project folder
 
-    let child = spawn(String.raw`npx --yes husky install && npx --yes husky add .husky/commit-msg "npx --yes commitlint --edit \"$1\""`, { shell: true });
-  
-
-    child.stdout.on("data", (data) => {
-      console.log(chalk.bold.green("SUCCESS"),'\n',data.toString())
-    });
-    child.stderr.on("data", (data) => {
-      console.log(chalk.bold.red("FAILURE"),'\n',data.toString())
-    });
-  
-    child.on("error", (error) => {
-      console.log(chalk.bold.red("Error Message"),'\n',  error.message);
-      console.log(chalk.bold.yellow("Please Make sure you run npm i commitlint-with-husky first!! before npx commitlint-with-husky"));
-    });
-    
-    child.on('exit', function (exitCode) {
-      console.log(chalk.cyan(`Process exited with code: ${ exitCode}`));
-    });
-
-  })();
+  try {
+    fs.writeFileSync(
+      path.resolve(userDirectory, "./.commitlintrc.json"),
+      JSON.stringify(configFile, null, 2)
+    );
+  } catch (error) {
+    console.log(
+      chalk.bold.red("Error copying over basic config file for Commitlint:\n"),
+      error
+    );
+  }
+  //install husky and git message hook
+  exec(huskyInstallation, { cwd: userDirectory }, (error) => {
+    if (error?.code) {
+      console.error(chalk.bold.red("An Error Occurred:\n\n"), error.message);
+      return;
+    }
+     
+  });
+  console.log(chalk.bold.green("commitlint-with-husky installed"))
+})();
